5eca9f58371433a731ac5d537dcb94641e8643b2
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-06-11 16:00:40 +0200

    added typing animation

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,22 @@
 import React from "react";
-
+import Typed from "react-typed"
 export default function HeroSection (){
 
   return(
     <div id="hero" className="hero-section">
     
-      <h2 className="hero-heading">We do websites right</h2>
+      <h2 className="hero-heading">{
+        <Typed
+      strings={[
+            "We build awesome products",
+            "We provide great software solutions",
+            "We promote black entreprenorship",
+          ]}
+          typeSpeed={100}
+          backSpeed={100}
+          loop
+        />
+      }</h2>
     </div>
   )
 }
